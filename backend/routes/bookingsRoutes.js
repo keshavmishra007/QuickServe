fef103b471887d4aabb6616a8dd4d35b87751fc8
keshavmishra007 <mkeshav700@gmail.com>
2026-05-05 15:37:29 +0530

    updating .env

--- backend/routes/bookingsRoutes.js
+++ backend/routes/bookingsRoutes.js
@@ -7,9 +7,9 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "quickserve12345@gmail.com",      // 👈 same gmail used for app password
-    pass: "ktsbcbziytzxxksq" // 👈 app password (no spaces)
-  }
+  user: process.env.EMAIL_USER,
+  pass: process.env.EMAIL_PASS,
+}
 });
 
 router.post("/", (req, res) => {
